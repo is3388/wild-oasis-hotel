ac379250e0cf377d136fa9ba6eea581b2405184a
is3388 <meiychin@yahoo.com>
2023-09-08 16:50:37 -0400

    Added error handling and image upload for cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,5 @@
 import supabase from './supabase';
+import { supabaseUrl } from './supabase';
 
 export async function getCabins() {
   // the code coming from supabase API Docs -> Tables & Views -> cabins and look for all rows
@@ -12,30 +13,42 @@ export async function getCabins() {
 }
 
 export async function deleteCabin(id) {
+  const { error } = await supabase.from('cabin').delete().eq('id', id);
 
-const { error } = await supabase
-.from('cabin')
-.delete()
-.eq('id', id)
-
-if (error) {
-  console.error(error);
-  throw new Error('Cabins could not be deleted');
-}
-return null;
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be deleted');
+  }
+  return null;
 }
 
 export async function createCabin(newCabin) {
-// name of the UI form fields match up with the column names in Supabase  
-const { data, error } = await supabase
-.from('cabin')
-.insert([newCabin])
-.select()
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    '/',
+    ''
+  );
 
-if (error) {
-  console.error(error);
-  throw new Error('Cabins could not be created');
-}
-return data;
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  // name of the UI form fields match up with the column names in Supabase
+  const { data, error } = await supabase
+    .from('cabin')
+    .insert([{...newCabin, image: imagePath }])
+    .select();
 
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be created');
+  }
+
+  // if new cabin created successful, upload image to supabase bucket by specifying the path and cabin-images is bucket name
+  const {error: storageError} = await supabase.storage.from('cabin-images').upload(imageName, newCabin.image)
+ 
+  // delete the new cabin if something goes wrong with the image upload
+  if(storageError) {
+    await supabase.from('cabin').delete().eq('id', data.id);
+    console.error(storageError);
+    throw new Error('Image upload failed and new cabin can not be created');
+  }
+  
+  return data;
 }
--- src/ui/FileInput.jsx
+++ src/ui/FileInput.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const FileInput = styled.input`
+const FileInput = styled.input.attrs({type: 'file'})` // specify file input not regular input
   font-size: 1.4rem;
   border-radius: var(--border-radius-sm);
 
